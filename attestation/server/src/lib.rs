a0fd72dd9601fe1200eddb80fb19d15b81900635
Roshan Raghupathy <git@roshanrags.com>
2025-12-11 22:01:40 +0530

    cargo fmt

--- attestation/server/src/lib.rs
+++ attestation/server/src/lib.rs
@@ -3,7 +3,6 @@ use aws_nitro_enclaves_nsm_api::driver as nsm_driver;
 use serde_bytes::ByteBuf;
 
 pub fn get_attestation_doc(pub_key: &[u8], user_data: &[u8]) -> Vec<u8> {
-
     let user_data_vec = if user_data.is_empty() {
         None
     } else {
@@ -16,11 +15,8 @@ pub fn get_attestation_doc(pub_key: &[u8], user_data: &[u8]) -> Vec<u8> {
         Some(pub_key.to_vec())
     };
 
-    return nitro_tpm_attest::attestation_document(
-        user_data_vec,
-        nonce_vec,
-        public_key_vec,
-    ).expect("Failed to generate attestation document");
+    return nitro_tpm_attest::attestation_document(user_data_vec, nonce_vec, public_key_vec)
+        .expect("Failed to generate attestation document");
 }
 
 pub fn get_hex_attestation_doc(pub_key: &[u8], user_data: &[u8]) -> String {
