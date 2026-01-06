a6fd1cbc7737d67cbdd323fe07146434fbc96ea1
Roshan Raghupathy <git@roshanrags.com>
2026-01-06 13:11:01 +0530

    redo the custom server based on attestation/server

--- attestation/server-custom/src/lib.rs
+++ attestation/server-custom/src/lib.rs
@@ -1,40 +1,22 @@
-use aws_nitro_enclaves_nsm_api::api::{Request, Response};
-use aws_nitro_enclaves_nsm_api::driver as nsm_driver;
-use serde_bytes::ByteBuf;
+use axum::http::StatusCode;
 
 pub fn get_attestation_doc(
     public_key: Option<&[u8]>,
     user_data: Option<&[u8]>,
     nonce: Option<&[u8]>,
-) -> Result<Vec<u8>, String> {
-    let public_key = public_key.map(ByteBuf::from);
-    let user_data = user_data.map(ByteBuf::from);
-    let nonce = nonce.map(ByteBuf::from);
-
-    let request = Request::Attestation {
-        public_key,
+) -> Result<Vec<u8>, (StatusCode, String)> {
+    return nitro_tpm_attest::attestation_document(
         user_data,
-        nonce,
-    };
-
-    let nsm_fd = nsm_driver::nsm_init();
-    let response = nsm_driver::nsm_process_request(nsm_fd, request);
-    nsm_driver::nsm_exit(nsm_fd);
-
-    match response {
-        Response::Attestation { document } => Ok(document),
-        _ => Err(format!(
-            "nsm driver returned invalid response: {:?}",
-            response
-        )),
-    }
+        None, // nonce
+        Some(pub_key.to_vec()),
+    )
+    .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("{e:?}")));
 }
 
 pub fn get_hex_attestation_doc(
     public_key: Option<&[u8]>,
     user_data: Option<&[u8]>,
     nonce: Option<&[u8]>,
-) -> Result<String, String> {
-    let attestation = get_attestation_doc(public_key, user_data, nonce);
-    attestation.map(hex::encode)
+) -> Result<String, (StatusCode, String)> {
+    get_attestation_doc(pub_key, user_data, nonce).map(hex::encode)
 }
