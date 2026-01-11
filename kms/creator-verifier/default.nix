9b020f7b1edbe81bb335aa178124ad25cd306363
Roshan Raghupathy <git@roshanrags.com>
2026-01-11 15:52:58 +0530

    rework nix build

--- kms/creator-verifier/default.nix
+++ kms/creator-verifier/default.nix
@@ -1,43 +1,24 @@
 {
   nixpkgs,
   systemConfig,
-  fenix,
-  naersk,
+  crane,
 }: let
   system = systemConfig.system;
   pkgs = nixpkgs.legacyPackages."${system}";
-  target = systemConfig.rust_target;
-  toolchain = with fenix.packages.${system};
-    combine [
-      stable.cargo
-      stable.rustc
-      targets.${target}.stable.rust-std
-    ];
-  naersk' = naersk.lib.${system}.override {
-    cargo = toolchain;
-    rustc = toolchain;
-  };
-  cc =
-    if systemConfig.static
-    then pkgs.pkgsStatic.stdenv.cc
-    else pkgs.stdenv.cc;
-in rec {
-  uncompressed = naersk'.buildPackage {
-    src = ./.;
-    CARGO_BUILD_TARGET = target;
-    TARGET_CC = "${cc}/bin/${cc.targetPrefix}cc";
-    nativeBuildInputs = [cc];
-  };
+  crane' = crane.mkLib pkgs;
+  commonArgs = {
+    strictDeps = true;
+    doCheck = false;
+    # DOES NOT run the check command
+    # short circuits it by running the true command instead
+    cargoCheckCommand = "true";
 
-  compressed =
-    pkgs.runCommand "compressed" {
-      nativeBuildInputs = [pkgs.upx];
-    } ''
-      mkdir -p $out/bin
-      cp ${uncompressed}/bin/* $out/bin/
-      chmod +w $out/bin/*
-      upx $out/bin/*
-    '';
-
-  default = compressed;
+    src = crane'.cleanCargoSource ./.;
+  };
+  deps = crane'.buildDepsOnly commonArgs;
+in {
+  default = crane'.buildPackage (commonArgs
+    // {
+      cargoArtifacts = deps;
+    });
 }
