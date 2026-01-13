de333ea39db2eff40a43e30f474e067b929d8229
Roshan Raghupathy <git@roshanrags.com>
2026-01-13 12:05:10 +0530

    limit regions

--- operator/setup-aws/src/compute_rates.rs
+++ operator/setup-aws/src/compute_rates.rs
@@ -12,34 +12,7 @@ use tokio_stream::StreamExt;
 use tracing::{error, info};
 use tracing_subscriber::EnvFilter;
 
-static REGIONS: [&str; 26] = [
-    "us-east-1",
-    "us-east-2",
-    "us-west-1",
-    "us-west-2",
-    "ca-central-1",
-    "sa-east-1",
-    "eu-north-1",
-    "eu-west-3",
-    "eu-west-2",
-    "eu-west-1",
-    "eu-central-1",
-    "eu-central-2",
-    "eu-south-1",
-    "eu-south-2",
-    "me-south-1",
-    "me-central-1",
-    "af-south-1",
-    "ap-south-1",
-    "ap-northeast-1",
-    "ap-northeast-2",
-    "ap-northeast-3",
-    "ap-southeast-1",
-    "ap-southeast-2",
-    "ap-southeast-3",
-    "ap-southeast-4",
-    "ap-east-1",
-];
+static REGIONS: [&str; 1] = ["ap-south-1"];
 
 static FAMILIES: [&str; 36] = [
     "m5", "m5a", "m5n", "m5zn", "m6a", "m6g", "m6i", "m6in", "m7g", "m7a", "m7i", "m8g", //
