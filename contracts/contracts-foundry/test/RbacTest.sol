1142d4dce2f86ee11c58711ffe4273aa2272eecb
Roshan Raghupathy <git@roshanrags.com>
2026-03-18 13:01:20 +0530

    add checks around events and revert reasons

--- contracts/contracts-foundry/test/RbacTest.sol
+++ contracts/contracts-foundry/test/RbacTest.sol
@@ -21,6 +21,8 @@ abstract contract RbacAdminTest is Test {
         assumeNotEqualAddress(_otherAdmin, admin)
     {
         vm.prank(admin);
+        vm.expectEmit();
+        emit IAccessControl.RoleGranted(DEFAULT_ADMIN_ROLE, _otherAdmin, admin);
         uut.grantRole(DEFAULT_ADMIN_ROLE, _otherAdmin);
         assertTrue(uut.hasRole(DEFAULT_ADMIN_ROLE, _otherAdmin));
     }
@@ -30,7 +32,11 @@ abstract contract RbacAdminTest is Test {
         assumeNotEqualAddress(_nonAdmin, admin)
     {
         vm.prank(_nonAdmin);
-        vm.expectRevert();
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccessControl.AccessControlUnauthorizedAccount.selector, _nonAdmin, DEFAULT_ADMIN_ROLE
+            )
+        );
         uut.grantRole(DEFAULT_ADMIN_ROLE, _nonAdmin);
     }
 
@@ -39,10 +45,14 @@ abstract contract RbacAdminTest is Test {
         assumeNotEqualAddress(_otherAdmin, admin)
     {
         vm.prank(admin);
+        vm.expectEmit();
+        emit IAccessControl.RoleGranted(DEFAULT_ADMIN_ROLE, _otherAdmin, admin);
         uut.grantRole(DEFAULT_ADMIN_ROLE, _otherAdmin);
         assertTrue(uut.hasRole(DEFAULT_ADMIN_ROLE, _otherAdmin));
 
         vm.prank(admin);
+        vm.expectEmit();
+        emit IAccessControl.RoleRevoked(DEFAULT_ADMIN_ROLE, _otherAdmin, admin);
         uut.revokeRole(DEFAULT_ADMIN_ROLE, _otherAdmin);
         assertFalse(uut.hasRole(DEFAULT_ADMIN_ROLE, _otherAdmin));
     }
@@ -53,16 +63,24 @@ abstract contract RbacAdminTest is Test {
         assumeNotEqualAddress(_nonAdmin, admin)
     {
         vm.prank(admin);
+        vm.expectEmit();
+        emit IAccessControl.RoleGranted(DEFAULT_ADMIN_ROLE, _otherAdmin, admin);
         uut.grantRole(DEFAULT_ADMIN_ROLE, _otherAdmin);
         assertTrue(uut.hasRole(DEFAULT_ADMIN_ROLE, _otherAdmin));
 
         vm.prank(_nonAdmin);
-        vm.expectRevert();
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccessControl.AccessControlUnauthorizedAccount.selector, _nonAdmin, DEFAULT_ADMIN_ROLE
+            )
+        );
         uut.revokeRole(DEFAULT_ADMIN_ROLE, _otherAdmin);
     }
 
     function test_AdminRole_AdminCanRenounceOwnAdminRole() public {
         vm.prank(admin);
+        vm.expectEmit();
+        emit IAccessControl.RoleRevoked(DEFAULT_ADMIN_ROLE, admin, admin);
         uut.renounceRole(DEFAULT_ADMIN_ROLE, admin);
         assertFalse(uut.hasRole(DEFAULT_ADMIN_ROLE, admin));
     }
@@ -76,7 +94,7 @@ abstract contract RbacAdminTest is Test {
         assertTrue(uut.hasRole(DEFAULT_ADMIN_ROLE, _otherAdmin));
 
         vm.prank(admin);
-        vm.expectRevert();
+        vm.expectRevert(IAccessControl.AccessControlBadConfirmation.selector);
         uut.renounceRole(DEFAULT_ADMIN_ROLE, _otherAdmin);
     }
 }
@@ -85,6 +103,7 @@ abstract contract RbacRoleTest is Test {
     IAccessControl public uut;
     address public admin;
     bytes32 public role;
+    bytes32 public constant DEFAULT_ADMIN_ROLE = bytes32(0);
 
     function _rbacRoleDeployContract(address _admin) internal virtual returns (IAccessControl, bytes32);
 
@@ -95,6 +114,8 @@ abstract contract RbacRoleTest is Test {
 
     function test_Role_AdminCanGrantRole(address _user) public {
         vm.prank(admin);
+        vm.expectEmit();
+        emit IAccessControl.RoleGranted(role, _user, admin);
         uut.grantRole(role, _user);
         assertTrue(uut.hasRole(role, _user));
     }
@@ -104,16 +125,24 @@ abstract contract RbacRoleTest is Test {
         assumeNotEqualAddress(_nonAdmin, admin)
     {
         vm.prank(_nonAdmin);
-        vm.expectRevert();
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccessControl.AccessControlUnauthorizedAccount.selector, _nonAdmin, DEFAULT_ADMIN_ROLE
+            )
+        );
         uut.grantRole(role, _user);
     }
 
     function test_Role_AdminCanRevokeRole(address _user) public {
         vm.prank(admin);
+        vm.expectEmit();
+        emit IAccessControl.RoleGranted(role, _user, admin);
         uut.grantRole(role, _user);
         assertTrue(uut.hasRole(role, _user));
 
         vm.prank(admin);
+        vm.expectEmit();
+        emit IAccessControl.RoleRevoked(role, _user, admin);
         uut.revokeRole(role, _user);
         assertFalse(uut.hasRole(role, _user));
     }
@@ -123,20 +152,30 @@ abstract contract RbacRoleTest is Test {
         assumeNotEqualAddress(_nonAdmin, admin)
     {
         vm.prank(admin);
+        vm.expectEmit();
+        emit IAccessControl.RoleGranted(role, _user, admin);
         uut.grantRole(role, _user);
         assertTrue(uut.hasRole(role, _user));
 
         vm.prank(_nonAdmin);
-        vm.expectRevert();
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccessControl.AccessControlUnauthorizedAccount.selector, _nonAdmin, DEFAULT_ADMIN_ROLE
+            )
+        );
         uut.revokeRole(role, _user);
     }
 
     function test_Role_RoleSignerCanRenounceOwnRole(address _user) public {
         vm.prank(admin);
+        vm.expectEmit();
+        emit IAccessControl.RoleGranted(role, _user, admin);
         uut.grantRole(role, _user);
         assertTrue(uut.hasRole(role, _user));
 
         vm.prank(_user);
+        vm.expectEmit();
+        emit IAccessControl.RoleRevoked(role, _user, _user);
         uut.renounceRole(role, _user);
         assertFalse(uut.hasRole(role, _user));
     }
