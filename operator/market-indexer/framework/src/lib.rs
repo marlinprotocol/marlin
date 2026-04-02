519941aca81a6b3b5c5f17542016003fe12282a6
Roshan Raghupathy <git@roshanrags.com>
2026-04-02 16:15:55 +0530

    rework indexer to look more like cvm-indexer

--- operator/market-indexer/framework/src/lib.rs
+++ operator/market-indexer/framework/src/lib.rs
@@ -1,53 +1,25 @@
 pub mod chain;
 pub mod events;
 pub(crate) mod models;
-pub(crate) mod repository;
 pub(crate) mod schema;
 
-use std::cmp::min;
-use std::time::Duration;
+use std::collections::HashSet;
 
 use anyhow::{Context, Result, anyhow};
-use diesel::PgConnection;
-use tracing::{debug, info, instrument, trace, warn};
+use diesel::{Connection, ExpressionMethods, PgConnection, QueryDsl, RunQueryDsl};
+use diesel_migrations::{EmbeddedMigrations, MigrationHarness, embed_migrations};
+use tracing::{info, instrument, trace, warn};
 
-use chain::{ChainHandler, transform_block_logs_into_records};
+use chain::ChainHandler;
 
-// Define generic trait for safe integer conversions
-pub trait SaturatingConvert<T> {
-    fn saturating_to(self) -> T;
-}
-
-// usize -> i64
-impl SaturatingConvert<i64> for usize {
-    fn saturating_to(self) -> i64 {
-        if self > i64::MAX as usize {
-            i64::MAX
-        } else {
-            self as i64
-        }
-    }
-}
-
-// u64 -> i64
-impl SaturatingConvert<i64> for u64 {
-    fn saturating_to(self) -> i64 {
-        if self > i64::MAX as u64 {
-            i64::MAX
-        } else {
-            self as i64
-        }
-    }
-}
-
-// i64 -> u64
-impl SaturatingConvert<u64> for i64 {
-    fn saturating_to(self) -> u64 {
-        if self < 0 { 0 } else { self as u64 }
-    }
-}
+use crate::{
+    events::JobEvent,
+    models::{JobEventName, JobEventRecord},
+};
 
 const BATCH_THRESHOLD: usize = 100;
+
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!("../framework/migrations");
 
 // TODO: add custom errors
 #[instrument(level = "info", skip_all, parent = None)]
@@ -55,121 +27,241 @@ pub fn run(
     conn: &mut PgConnection,
     rpc_client: &mut impl ChainHandler,
     provider: String,
-    mut start_block: Option<i64>,
+    start_block: Option<u64>,
     range_size: u64,
 ) -> Result<()> {
-    let mut last_processed_block_id = repository::get_last_processed_block(conn)
-        .context("Missing last processed block (possible DB corruption)")?;
+    // apply migrations
+    conn.run_pending_migrations(MIGRATIONS)
+        .map_err(|e| anyhow::anyhow!("Migrations failed: {}", e))?;
+
+    // fetch last updated block from the db
+    let mut last_updated = schema::sync::table
+        .select(schema::sync::block)
+        .limit(1)
+        .load::<i64>(conn)
+        .context("failed to fetch last updated block")?
+        .into_iter()
+        .last()
+        .ok_or(anyhow!(
+            "no last updated block found, should never happen unless the database is corrupted"
+        ))? as u64;
 
     if let Some(block) = start_block {
-        if block <= last_processed_block_id {
+        if block <= last_updated {
             warn!(
-                "Provided start block {} is behind the last processed block {}, starting from the later!",
-                block, last_processed_block_id
+                "Provided start block {} is behind the last processed block {}, starting from the latter!",
+                block, last_updated
             );
-            start_block = None;
         } else {
-            last_processed_block_id = block - 1;
+            last_updated = block - 1;
         }
     }
 
-    info!(
-        last_processed_block_id,
-        "Resuming from last processed block"
-    );
-
-    let chain_id = rpc_client
-        .fetch_chain_id()
-        .context("RPC chain ID fetch failed")?;
-
-    let updated = repository::update_indexer_state(conn, chain_id.clone(), start_block)
-        .context("Failed to update indexer state in the DB")?;
-
-    info!("Indexer state updated: {}", updated == 1);
-
-    if range_size == 0 {
-        return Err(anyhow!("Range size must not be zero"));
-    }
+    info!(block = last_updated, "Resuming from last processed block");
 
     let mut active_job_ids =
-        repository::get_active_jobs(conn).context("Failed to fetch active job IDs from the DB")?;
+        get_active_jobs(conn).context("Failed to fetch active job IDs from the DB")?;
 
     loop {
+        // fetch latest block from the rpc
         let latest_block = rpc_client
             .fetch_latest_block()
             .context("RPC latest block fetch failed")?;
-        let latest_block_i64: i64 = latest_block.saturating_to();
 
-        debug!(latest_block, "Fetched latest block from RPC");
+        info!(block = latest_block, "latest block");
 
-        if latest_block_i64 < last_processed_block_id {
-            // warn!(db_block = last_processed_block_id, rpc_block = latest_block_i64, "RPC is behind DB (possible rollback)");
+        // should not really ever be true
+        // effectively means the rpc was rolled back
+        if latest_block < last_updated {
             return Err(anyhow!(
                 "RPC {} is behind DB {} (possible rollback)",
-                latest_block_i64,
-                last_processed_block_id
+                latest_block,
+                last_updated
             ));
         }
 
-        if latest_block_i64 == last_processed_block_id {
-            trace!("Up-to-date with RPC, sleeping 5s");
-            std::thread::sleep(Duration::from_secs(5));
-            continue;
-        }
+        // start from the next block to what has already been processed
+        let start_block = last_updated + 1;
+        // cap block range using range_size
+        // might need some babysitting during initial sync
+        let end_block = std::cmp::min(start_block + range_size - 1, latest_block);
 
-        let start_block: u64 = (last_processed_block_id + 1).saturating_to();
-        let end_block = min(start_block + range_size - 1, latest_block);
         info!(start_block, end_block, "Fetching new block range");
 
         let block_logs = rpc_client
-            .fetch_logs_and_group_by_block(start_block, end_block)
+            .fetch_logs(start_block, end_block)
             .context("Failed to fetch logs from the chain")?;
+
         info!(start_block, end_block, "Processing block range");
 
-        let mut end_block_num = last_processed_block_id;
-        let mut batch_records = Vec::new();
-
-        for block_number in start_block..=end_block {
-            let empty = Vec::new();
-
-            let records = transform_block_logs_into_records(
-                &provider,
-                block_logs.get(&block_number).unwrap_or(&empty),
-                &mut active_job_ids,
-            )
+        let records = transform_events_into_records(&provider, &block_logs, &mut active_job_ids)
             .context("Failed to transform block logs into DB records")?;
 
-            debug!(
-                block_number,
-                events_count = records.len(),
-                "Processing block logs"
-            );
+        // execute db writes within a transaction for consistency
+        // NOTE: diesel transactions are synchronous, async is not allowed inside
+        // might be limiting for certain things like making rpc queries while processing logs
+        // using a temporary tokio runtime is a possibility
+        conn.transaction(|conn| {
+            diesel::insert_into(schema::job_events::table)
+                .values(&records)
+                .execute(conn)
+                .context("failed to insert records")?;
+            diesel::update(schema::sync::table)
+                .set(schema::sync::block.eq(end_block as i64))
+                .execute(conn)
+                .context("failed to update latest block")
+        })?;
 
-            end_block_num = block_number.saturating_to();
-            batch_records.extend(records);
-
-            if batch_records.len() >= BATCH_THRESHOLD {
-                let (inserted_batch, updated) =
-                    repository::insert_batch(conn, batch_records.clone(), end_block_num)
-                        .context("DB insert failed for block batch")?;
-
-                debug!(end_block_num, inserted_batch, "Inserted block logs");
-                trace!("Last processed block updated: {}", updated == 1);
-
-                batch_records.clear();
-                last_processed_block_id = end_block_num;
-            }
-        }
-
-        if end_block_num > last_processed_block_id {
-            let (inserted_batch, updated) =
-                repository::insert_batch(conn, batch_records.clone(), end_block_num)
-                    .context("DB insert failed for block batch")?;
-
-            debug!(end_block_num, inserted_batch, "Inserted block logs");
-            trace!("Last processed block updated: {}", updated == 1);
-        }
-
-        last_processed_block_id = end_block_num;
+        last_updated = end_block;
     }
+}
+
+fn get_active_jobs(conn: &mut PgConnection) -> Result<HashSet<u64>> {
+    use crate::models::JobEventName;
+    use schema::job_events;
+
+    let opened_jobs = job_events::table
+        .select(job_events::job_id)
+        .filter(job_events::event_name.eq(JobEventName::Opened))
+        .load::<i64>(conn)
+        .context("Failed to query opened jobs")?;
+
+    let closed_jobs = job_events::table
+        .select(job_events::job_id)
+        .filter(job_events::event_name.eq(JobEventName::Closed))
+        .load::<i64>(conn)
+        .context("Failed to query closed jobs")?;
+
+    let mut active_jobs_set: HashSet<u64> = opened_jobs.into_iter().map(|x| x as u64).collect();
+    for closed_job in closed_jobs {
+        active_jobs_set.remove(&(closed_job as u64));
+    }
+
+    Ok(active_jobs_set)
+}
+
+fn transform_events_into_records(
+    provider: &str,
+    job_events: &[JobEvent],
+    active_jobs: &mut HashSet<u64>,
+) -> Result<Vec<JobEventRecord>> {
+    let mut job_event_records = vec![];
+
+    for job_event in job_events.iter() {
+        match job_event {
+            JobEvent::Opened(event) => {
+                // Check if provider matches the target
+                if !event.provider.eq_ignore_ascii_case(provider) {
+                    continue;
+                }
+
+                active_jobs.insert(event.job_id.clone());
+                job_event_records.push(JobEventRecord {
+                    job_id: event.job_id as i64,
+                    event_name: JobEventName::Opened,
+                    event_data: serde_json::to_value(event)
+                        .context("Failed to JSON serialize JobOpened event data")?,
+                });
+            }
+            JobEvent::Closed(event) => {
+                if !active_jobs.contains(&event.job_id) {
+                    continue;
+                }
+
+                active_jobs.remove(&event.job_id);
+                job_event_records.push(JobEventRecord {
+                    job_id: event.job_id as i64,
+                    event_name: JobEventName::Closed,
+                    event_data: serde_json::to_value(event)
+                        .context("Failed to JSON serialize JobClosed event data")?,
+                });
+            }
+            JobEvent::Settled(event) => {
+                if !active_jobs.contains(&event.job_id) {
+                    continue;
+                }
+
+                job_event_records.push(JobEventRecord {
+                    job_id: event.job_id as i64,
+                    event_name: JobEventName::Settled,
+                    event_data: serde_json::to_value(event)
+                        .context("Failed to JSON serialize JobSettled event data")?,
+                });
+            }
+            JobEvent::Deposited(event) => {
+                if !active_jobs.contains(&event.job_id) {
+                    continue;
+                }
+
+                job_event_records.push(JobEventRecord {
+                    job_id: event.job_id as i64,
+                    event_name: JobEventName::Deposited,
+                    event_data: serde_json::to_value(event)
+                        .context("Failed to JSON serialize JobDeposited event data")?,
+                });
+            }
+            JobEvent::Withdrew(event) => {
+                if !active_jobs.contains(&event.job_id) {
+                    continue;
+                }
+
+                job_event_records.push(JobEventRecord {
+                    job_id: event.job_id as i64,
+                    event_name: JobEventName::Withdrew,
+                    event_data: serde_json::to_value(event)
+                        .context("Failed to JSON serialize JobWithdrew event data")?,
+                });
+            }
+            JobEvent::ReviseRateInitiated(event) => {
+                if !active_jobs.contains(&event.job_id) {
+                    continue;
+                }
+
+                job_event_records.push(JobEventRecord {
+                    job_id: event.job_id as i64,
+                    event_name: JobEventName::ReviseRateInitiated,
+                    event_data: serde_json::to_value(event)
+                        .context("Failed to JSON serialize JobReviseRateInitiated event data")?,
+                });
+            }
+            JobEvent::ReviseRateCancelled(event) => {
+                if !active_jobs.contains(&event.job_id) {
+                    continue;
+                }
+
+                job_event_records.push(JobEventRecord {
+                    job_id: event.job_id as i64,
+                    event_name: JobEventName::ReviseRateCancelled,
+                    event_data: serde_json::to_value(event)
+                        .context("Failed to JSON serialize JobReviseRateCancelled event data")?,
+                });
+            }
+            JobEvent::ReviseRateFinalized(event) => {
+                if !active_jobs.contains(&event.job_id) {
+                    continue;
+                }
+
+                job_event_records.push(JobEventRecord {
+                    job_id: event.job_id as i64,
+                    event_name: JobEventName::ReviseRateFinalized,
+                    event_data: serde_json::to_value(event)
+                        .context("Failed to JSON serialize JobReviseRateFinalized event data")?,
+                });
+            }
+            JobEvent::MetadataUpdated(event) => {
+                if !active_jobs.contains(&event.job_id) {
+                    continue;
+                }
+
+                job_event_records.push(JobEventRecord {
+                    job_id: event.job_id as i64,
+                    event_name: JobEventName::MetadataUpdated,
+                    event_data: serde_json::to_value(event)
+                        .context("Failed to JSON serialize JobMetadataUpdated event data")?,
+                });
+            }
+        };
+    }
+
+    Ok(job_event_records)
 }
