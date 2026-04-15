79642e734ded2fbafb0a6e037955917515b2da53
Roshan Raghupathy <git@roshanrags.com>
2026-04-15 14:36:17 +0530

    add format function for rate

--- cli/marlin-cvm/src/utils/mod.rs
+++ cli/marlin-cvm/src/utils/mod.rs
@@ -5,3 +5,7 @@ pub mod bandwidth;
 pub fn format_usdc(value: u64) -> f64 {
     value as f64 / 10f64.powi(6)
 }
+
+pub fn format_rate(value: u64) -> f64 {
+    value as f64 * 3600f64 / 10f64.powi(EXTRA_DECIMALS as i32 + 6)
+}
