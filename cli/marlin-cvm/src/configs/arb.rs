a64ca3890ef50f8cdc2150b264d39a101952d7d3
Roshan Raghupathy <git@roshanrags.com>
2026-04-17 13:22:29 +0530

    use official rpc

--- cli/marlin-cvm/src/configs/arb.rs
+++ cli/marlin-cvm/src/configs/arb.rs
@@ -3,7 +3,7 @@ pub const MARKET_ADDRESS: &str = "0x02301739bF5c1AA8a6b484CaA4daE64803cbC207";
 pub const USDC_ADDRESS: &str = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831";
 
 // RPC URLs
-pub const RPC_URL: &str = "https://1rpc.io/arb";
+pub const RPC_URL: &str = "https://arb1.arbitrum.io/rpc";
 
 // Default operators
 pub const DEFAULT_OPERATOR_ADDRESS: &str = "0xe10Fa12f580e660Ecd593Ea4119ceBC90509D642";
