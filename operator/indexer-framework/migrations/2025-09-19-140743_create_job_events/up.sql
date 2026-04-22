2764766eec6d34727955882eb5a4c9c8be32d69e
Roshan Raghupathy <git@roshanrags.com>
2026-04-22 17:22:34 +0530

    convert event data to bytes instead of json, we can store binary data directly

--- operator/indexer-framework/migrations/2025-09-19-140743_create_job_events/up.sql
+++ operator/indexer-framework/migrations/2025-09-19-140743_create_job_events/up.sql
@@ -12,7 +12,7 @@ CREATE TABLE job_events (
     id BIGSERIAL PRIMARY KEY,
     job_id BIGINT NOT NULL,
     event_name event_name NOT NULL,
-    event_data JSONB NOT NULL
+    event_data BYTEA NOT NULL
 );
 
 CREATE INDEX idx_job_events_event_name_job_id ON job_events (event_name, job_id);
