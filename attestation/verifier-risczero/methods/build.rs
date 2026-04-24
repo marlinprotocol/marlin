b3e93ad2b93ef9441370b9e22de6efdea1592916
Roshan Raghupathy <git@roshanrags.com>
2026-04-24 17:15:21 +0530

    enable docker build for consistency

--- attestation/verifier-risczero/methods/build.rs
+++ attestation/verifier-risczero/methods/build.rs
@@ -1,3 +1,14 @@
+use std::collections::HashMap;
+
+use risc0_build::{DockerOptionsBuilder, GuestOptionsBuilder};
+
 fn main() {
-    risc0_build::embed_methods();
+    let docker_options = DockerOptionsBuilder::default().build().unwrap();
+    let guest_options = GuestOptionsBuilder::default()
+        .use_docker(docker_options)
+        .build()
+        .unwrap();
+    let mut options = HashMap::new();
+    options.insert("guest", guest_options);
+    risc0_build::embed_methods_with_options(options);
 }
