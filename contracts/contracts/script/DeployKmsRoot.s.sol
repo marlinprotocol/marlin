f8eb85e56cd571465ac323d1f5d6092e2e0ec4f7
Roshan Raghupathy <git@roshanrags.com>
2026-04-27 12:23:51 +0530

    add script to deploy kms with a custom name

---
+++ contracts/contracts/script/DeployKmsRoot.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {DeployScript} from "./DeployScript.s.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {KmsRoot} from "../src/kms/KmsRoot.sol";
+
+contract DeployKmsRoot is DeployScript {
+    constructor() DeployScript(vm.envString("KMS_ROOT_NAME")) {}
+
+    function run() public {
+        // collect params
+        address _admin = _getDeployed("KmsAdmin");
+        address _verifier = _getDeployed("RiscZeroVerifier");
+        bytes32 _guestId = _getConfigBytes32("guestId", "Kms");
+        bytes memory _rootKey = _getConfigBytes("rootKey", "Kms");
+        uint256 _maxAgeMs = _getConfigUint256("maxAgeMs", "Kms");
+        bytes32 _imageId = _getConfigBytes32("imageId", "Kms");
+
+        // deploy
+        vm.startBroadcast();
+        address _proxy = Upgrades.deployUUPSProxy(
+            "KmsRoot.sol",
+            abi.encodeCall(
+                KmsRoot.initialize, (_admin, _admin, _admin, _verifier, _guestId, _rootKey, _maxAgeMs, _imageId)
+            )
+        );
+        vm.stopBroadcast();
+
+        // record address
+        _setDeployed(_proxy);
+    }
+}
